57dd0025410f2a2b190bb53d594e78508444973d
Filip Weiss <me@fiws.net>
2013-10-15 17:41:10 +0200

    add gruntfile and jshint config

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			files: ['gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+			options:{
+				jshintrc: ".jshintrc"
+			}
+		},
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec'
+				},
+				src: ['test/**/*.js']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-mocha-test');
+
+	grunt.registerTask('test', ['jshint', 'mochaTest']);
+};
